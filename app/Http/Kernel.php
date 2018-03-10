059f679a5c68e1102643abba462910fa29a45166
Xander Luciano <vipercodegames@gmail.com>
2018-03-09 22:49:59 -0500

    attach passport token to middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,9 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            // So we can consume our own API - pass a token on through.
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
 
         'api' => [
