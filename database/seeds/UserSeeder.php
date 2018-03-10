5c966966aedec941065298be549fc6269a28e83b
Xander Luciano <ViperCodeGames@gmail.com>
2018-03-09 19:20:06 -0800

    Added database seeder for users

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Xander',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('123'),
+        ]);
+    }
+}
